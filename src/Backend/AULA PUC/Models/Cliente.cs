ee2fc8363c82960f291d0dc15e7f7bf78e872721
PamelaCrivellari <89881486+PamelaCrivellari@users.noreply.github.com>
2022-05-04 23:44:30 -0300

    adicionei model cliente

---
+++ src/Backend/AULA PUC/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hora_do_Banho_Teste.Models
+{
+    public class Cliente
+    {
+        [Table("Cliente")]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Obrigatório informar o nome!")]
+        public string Nome_Sobrenome { get; set; }
+        [Key]
+        public char CPF { get; set; }
+        public DateOnly Data_Nascimento { get; set; }
+        [Required(ErrorMessage ="Obrigatório informar o telefone!")]
+        public int Telefone { get; set; }
+        [Required(ErrorMessage ="Obrigatório informar o e-mail!")]
+        public char Email { get; set; }
+        public char Endereco { get; set; }
+        [Required(ErrorMessage ="Informe sua senha!")]
+        public char Senha { get; set; }
+    }
+}
