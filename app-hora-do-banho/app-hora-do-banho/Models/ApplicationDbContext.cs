d6e196e6effa567833d96a2af7458c4623ecdfd2
patricia <pnfreitas2014@gmail.com>
2022-06-12 22:38:04 -0300

    added app-hora-do-banho folder

---
+++ app-hora-do-banho/app-hora-do-banho/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace app_hora_do_banho.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+
+}
---
+++ app-hora-do-banho/app-hora-do-banho/Models/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_hora_do_banho.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Obrigatório informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o telefone")]
+        public string Telefone { get; set; }
+
+        public string CEP { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o e-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o perfil")]
+        public Perfil Perfil { get; set; }
+    }
+    public enum Perfil
+    {
+        PetShop,
+        Cliente
+    }
+
+    
+}
+
